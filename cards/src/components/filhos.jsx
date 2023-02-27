11a2f243a63ec6393bf0f4eb523e44fa2403a500
arkannun <arkannun@gmail.com>
2023-02-27 15:14:10 -0300

    Criação do componente filhos

---
+++ cards/src/components/filhos.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const filhos = props =>
+    <div>
+        <h1>Filhos</h1>
+        {props.children}
+    </div>
+
+export default filhos
--- cards/src/index.js
+++ cards/src/index.js
@@ -2,14 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Primeiro from './components/primeiro'
 import Parametro from './components/parametro'
+import Filhos from './components/filhos'
 
 import './index.css'
 
 ReactDOM.render(
-    <React.Fragment>
-        <Primeiro/>
+    <>
+        <Primeiro />
         <Parametro nome="Maks" idade="42" />
-    </React.Fragment>
-    
-    ,document.getElementById('root')
+        <Filhos>
+            <ul>
+                <li className="1">Maks</li>
+                <li className="2">Jaqueline</li>
+                <li className="3">Debora</li>
+                <li className="4">Rhyan</li>
+                <li className="5">Papa</li>
+            </ul>
+        </Filhos>
+    </>
+
+    , document.getElementById('root')
 )
